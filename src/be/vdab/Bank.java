2d8f33451dee1a825eb57a818801362e785eb13b
miked <miked@MikeLaptop>
2017-04-14 00:14:32 +0200

    V1.5

--- src/be/vdab/Bank.java
+++ src/be/vdab/Bank.java
@@ -1,5 +1,6 @@
-// JDBC test By Mike D.
 package be.vdab;
+
+/* JDBC test By Mike D. V1.5*/
 
 import java.math.BigDecimal;
 import java.sql.Connection;
@@ -12,132 +13,243 @@ import java.util.Scanner;
 public class Bank {
 
 	private static Scanner scanner;
-	private static final String URL = "jdbc:mysql://localhost/bank?useSSL=false";
-	private static final String USER = "cursist";
-	private static final String PASSWORD = "cursist";
+	private static int keuze;
+	private static final String DB = "jdbc:mysql://localhost/";
+	private static final String DB_NAME = "bank";
+	private static final String DB_SECURE = "?useSSL=false";
+	private static final String USER = "root";
+	private static final String PASSWORD = "vdab";
 	private static final String SQL_NIEUW_REK = "INSERT INTO rekeningen (RekeningNr) VALUES (?)";
 	private static final String SQL_SALDO = "SELECT saldo FROM rekeningen WHERE RekeningNr = ?";
-	private static final String SQL_OVER_MIN = "UPDATE rekeningen set Saldo = saldo - ? WHERE RekeningNr=?";
-	private static final String SQL_OVER_PLUS = "UPDATE rekeningen set Saldo = saldo + ? WHERE RekeningNr=?";
+	private static final String SQL_OVER_MIN = "UPDATE rekeningen set saldo = saldo - ? WHERE RekeningNr=?";
+	private static final String SQL_OVER_PLUS = "UPDATE rekeningen set saldo = saldo + ? WHERE RekeningNr=?";
+	private static final String SQL_CREATE_DB = "CREATE DATABASE " + DB_NAME;
+	private static final String SQL_CREATE_TB = "CREATE TABLE rekeningen "
+											+ "(ID INTEGER(5) auto_increment primary key not null, RekeningNr BIGINT not null, saldo DECIMAL(12,2) not null DEFAULT 0)";
 
 	public static void main(String[] args) {
 		
-		scanner = new Scanner(System.in);
-		System.out.println("1. Nieuwe rekening\n2. Saldo consulteren \n3.Overschrijven\nTyp je keuze (1,2 of 3)");
-		int keuze = scanner.nextInt();
-			
-		try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)){
+		System.out.println("-------------------------\n|   TEST JDBC MIKE D.   |\n-------------------------");
 		
-	
+		scanner = new Scanner(System.in);
+		toonMenu();
+			
+		while (keuze != 0){
+		
 		switch (keuze) {
 		// Nieuwe rekening maken
 		case 1:
-			System.out.println("Nieuwe rekening\n---------------");
-			System.out.println("Wat is het nieuwe rekening nummer ? (12cijfers)");
-			long NrekNR = scanner.nextLong();
-			Rekening Nieuw = new Rekening(NrekNR);
-			
-			if(Nieuw != null){
-				try(PreparedStatement statement1 = connection.prepareStatement(SQL_NIEUW_REK)){
+			try(Connection connection = DriverManager.getConnection(DB + DB_NAME + DB_SECURE, USER, PASSWORD)){
+				
+				subMenu("Nieuwe rekening");
+				rekVragen();
+				long NrekNR = scanner.nextLong();
+				
+				if (NrekNR < 100_000_000_000L || NrekNR > 999_999_999_999L){
+					fout("Rekening nummer moet een getal met 12 cijfers zijn");
+				} else if (!chekRekNr(NrekNR)) {
+					fout("Foutief rekening nummer");
+				} else {
 					
-					statement1.setLong(1, NrekNR);
-					statement1.executeUpdate();
-					System.out.println("Nieuwe rekening is aangemaakt.");
+					try(PreparedStatement statement1 = connection.prepareStatement(SQL_NIEUW_REK)){
+					
+						statement1.setLong(1, NrekNR);
+						statement1.executeUpdate();
+						System.out.println("Nieuwe rekening is aangemaakt.");
+					}
 				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.exit(0);
 			}
+			toonMenu();
 			break;
 		
 		//Saldo controleren
 		case 2:
-			System.out.println("Saldo consulteren\n-----------------");
-			System.out.println("Wat is het rekening nummer ? (12cijfers)");
-			NrekNR = scanner.nextLong();
+			try(Connection connection = DriverManager.getConnection(DB + DB_NAME + DB_SECURE, USER, PASSWORD)){
+				subMenu("Saldo consulteren");
+				rekVragen();
+				long NrekNR = scanner.nextLong();
 			
-			try ( PreparedStatement statement2 = connection.prepareStatement(SQL_SALDO)) {
+				try ( PreparedStatement statement2 = connection.prepareStatement(SQL_SALDO)) {
 
-				statement2.setLong(1, NrekNR);
+					statement2.setLong(1, NrekNR);
 				
-					try(ResultSet resultSet = statement2.executeQuery()){
-						if (resultSet.next()){
-							System.out.println("€" + resultSet.getBigDecimal("saldo"));
-						} else if (!resultSet.next()){
-							System.err.println("Dit rekening nummer staat niet in onze database");
+						try(ResultSet resultSet = statement2.executeQuery()){
+							if (resultSet.next()){
+								System.out.println("€" + resultSet.getBigDecimal("saldo"));
+							} else if (!resultSet.next()){
+								fout("Dit rekening nummer staat niet in onze database");
+							}
 						}
-					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.exit(0);
 			}
+			toonMenu();
 			break;
 		
 		//Overschrijving
 		case 3:
-			System.out.println("Overschrijving\n---------------");
-			System.out.println("Vanaf welke rekening wil je een overschrijving doen ? (12cijfers)");
-			long VanNrekNR = scanner.nextLong();
-			System.out.println("Naar welke rekening wil je de overschrijving doen ? (12cijfers)");
-			long NaarNrekNR = scanner.nextLong();
-			System.out.println("Hoeveel wilt u overschrijven ?");
-			BigDecimal bedrag = scanner.nextBigDecimal();
-			boolean fout = false;
-			BigDecimal onderNul = BigDecimal.ZERO;
+			try(Connection connection = DriverManager.getConnection(DB + DB_NAME + DB_SECURE, USER, PASSWORD)){
+				subMenu("Overschrijving");
+				
+				rekVragen("Van : ");
+				long VanNrekNR = scanner.nextLong();
+				rekVragen("Naar : ");
+				long NaarNrekNR = scanner.nextLong();
+				System.out.println("Hoeveel wilt u overschrijven ?");
+				BigDecimal bedrag = scanner.nextBigDecimal();
+				
+				boolean fout = false;
+				BigDecimal onderNul = BigDecimal.ZERO;
 			
-			if (VanNrekNR == NaarNrekNR){
-				System.err.println("Je kan niet naar je zelf overschrijven.");
-			} else {
+				if (VanNrekNR == NaarNrekNR){
+					fout("Je kan niet naar je zelf overschrijven.");
+				} else {
 			
-			try ( PreparedStatement statement3 = connection.prepareStatement(SQL_SALDO)) {
+					try ( PreparedStatement statement3 = connection.prepareStatement(SQL_SALDO)) {
 
-					//Controle van start rekening + saldo
-					statement3.setLong(1, VanNrekNR);
-					try(ResultSet resultSet = statement3.executeQuery()){
-						if (resultSet.next()){
-							if ((resultSet.getBigDecimal("saldo").compareTo(bedrag) < 0) || bedrag.compareTo(onderNul) < 0){
-								System.err.println("Het saldo op de rekening is niet voldoende. Of je hebt een negatief getal ingegeven.");
+						//Controle van start rekening + saldo
+						statement3.setLong(1, VanNrekNR);
+						try(ResultSet resultSet = statement3.executeQuery()){
+							if (resultSet.next()){
+								if ((resultSet.getBigDecimal("saldo").compareTo(bedrag) < 0) || bedrag.compareTo(onderNul) < 0){
+									fout("Het saldo op de rekening is niet voldoende. Of je hebt een negatief getal ingegeven.");
+									fout = true;
+								}
+							} else if (!resultSet.next()){
+								fout("Jouw rekening nummer staat niet in onze database.");
 								fout = true;
 							}
-						} else if (!resultSet.next()){
-							System.err.println("Jouw rekening nummer staat niet in onze database.");
-							fout = true;
 						}
-					}
 						
-					//Controle van de doel rekening
-					statement3.setLong(1, NaarNrekNR);
-					try(ResultSet resultSet = statement3.executeQuery()){
-						if (!resultSet.next()){	
-							System.err.println("Doel rekening nummer staat niet in onze database.");
-							fout = true;
+						//Controle van de doel rekening
+						statement3.setLong(1, NaarNrekNR);
+						try(ResultSet resultSet = statement3.executeQuery()){
+							if (!resultSet.next()){	
+								fout("Doel rekening nummer staat niet in onze database.");
+								fout = true;
+							}
 						}
 					}
 					
-					//De overschrijving zelf
-					if (!fout){
-					try(
-						PreparedStatement statement4 = connection.prepareStatement(SQL_OVER_MIN);
-						PreparedStatement statement5 = connection.prepareStatement(SQL_OVER_PLUS)
-					){
-						connection.setAutoCommit(false);
-						statement4.setBigDecimal(1, bedrag);
-						statement5.setBigDecimal(1, bedrag);
-						statement4.setLong(2, VanNrekNR);
-						statement5.setLong(2, NaarNrekNR);
-						statement4.executeUpdate();
-						statement5.executeUpdate();
-						connection.commit();
-						System.out.println("Er is €" + bedrag + " overgeschreven");
-					}
-					}	
+						//De overschrijving zelf
+						if (!fout){
+							try(
+							PreparedStatement statement4 = connection.prepareStatement(SQL_OVER_MIN);
+							PreparedStatement statement5 = connection.prepareStatement(SQL_OVER_PLUS)
+							){
+								connection.setAutoCommit(false);
+								statement4.setBigDecimal(1, bedrag);
+								statement5.setBigDecimal(1, bedrag);
+								statement4.setLong(2, VanNrekNR); 
+								statement5.setLong(2, NaarNrekNR);
+								statement4.executeUpdate();
+								statement5.executeUpdate();
+								connection.commit();
+								System.out.println("Er is €" + bedrag + " overgeschreven");
+							}
+						}	
 			}
-			}
-			break;
-
-		//keuze niet 1, 2 of 3
-		default:
-			break;
-		}
-		
+				
 		} catch (SQLException e) {
 			e.printStackTrace();
+			System.exit(0);
+		}
+			toonMenu();
+			break;
+			
+		//Database en tabellen maken
+		case 4:
+			subMenu("DataBase Maken");
+			
+			//Database maken
+			try(
+				Connection connection2 = DriverManager.getConnection(DB + DB_SECURE, USER, PASSWORD);
+				PreparedStatement statement6 = connection2.prepareStatement(SQL_CREATE_DB)
+			){
+				statement6.executeUpdate();
+				System.out.println("Database aangemaakt");
+			} catch (SQLException e) {
+				if (e.getErrorCode() == 1007){
+					fout("Database is al gemaakt");
+				} else {
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+			
+			//Tabellen maken
+			try(
+				Connection connection = DriverManager.getConnection(DB + DB_NAME + DB_SECURE, USER, PASSWORD);
+				PreparedStatement statement7 = connection.prepareStatement(SQL_CREATE_TB)
+			){
+				statement7.executeUpdate();
+				System.out.println("Tabellen aangemaakt");
+			} catch (SQLException e) {
+				if (e.getErrorCode() == 1050){
+					fout("Tabbellen zijn al gemaakt.");
+				} else {
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+			
+			toonMenu();
+			break;
+
+		//keuze niet 1, 2, 4, 0
+		default:
+			fout("Je moet binnen de keuze mogelijkheden bijven.");
+			toonMenu();
+			break;
+		}
+	
 		}
 	}
 	
+	//Methodes
+	
+	//Hoofd Menu
+	public static void toonMenu() {
+		System.out.println(
+				  "\n-------------------------\n"
+				+ "| 1. Nieuwe rekening    |\n"
+				+ "| 2. Saldo consulteren  |\n"
+				+ "| 3. Overschrijven      |\n"
+				+ "| 4. Database maken     |\n"
+				+ "| 0. Stoppen            |\n"
+				+ "-------------------------"
+				);
+		keuze = scanner.nextInt();
+	}
+	
+	//Sub menu
+	private static void subMenu(String titel) {
+		System.out.println(titel + "\n-------------------------");
+	}
+	
+	//Rekening nr vragen
+	private static void rekVragen(String extra) {
+		System.out.println(extra + "Geef rekening nummer ? (12cijfers)");
+	}
+	private static void rekVragen() {
+		System.out.println("Geef rekening nummer ? (12cijfers)");
+	}
+	
+	//Fouten
+	private static void fout(String fout) {
+		System.err.println( fout );
+	}
+	
+	private static boolean chekRekNr(Long reknr) {
+		long eerste10 = reknr/100;
+		long laaste2 = reknr % 10;
+		long controle = eerste10 % 97;
+		return controle == laaste2;
+	}
 }
 
